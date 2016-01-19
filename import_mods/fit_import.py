0bbc598137facb74e7135c0a4f679f2b536b8292
Jack Yates <j.yates.13@googlemail.com>
2016-01-19 01:14:26 +0000

    Started to add module for parsing FIT files - unfortunately fitparse, the appropriate python module, is no longer being updated. Something else required.

---
+++ import_mods/fit_import.py
@@ -0,0 +1,28 @@
+## =============================================================================================================
+## PROGRAM NAME: FIT IMPORT
+## CREATED BY: Jack	(GitHub: jyates13)
+## CREATED ON: 19/01/2015
+##==============================================================================================================
+## DESCRIPTION:
+##  Load and deal with .FIT files. 
+##==============================================================================================================
+## USED BY:
+##  Name the modules that uses this module
+##==============================================================================================================
+
+def load_fit_file(filename):
+    '''
+        This module parses the data from the .fit file given to it and returns a list if instances
+        Each instance contains lat,lon,elevation,time and a few other parameters. This list of
+        instances can be used to extract more readable data (eg. get a list of latitudes)
+
+        The returned list will be a long list (in the 1000's depending on the activity).
+        Each element is an instance of gps data.
+    '''
+
+    return None
+
+
+## =============================================================================================================
+## END OF PROGRAM
+## =============================================================================================================
